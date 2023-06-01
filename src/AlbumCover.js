ce5e2bd1b85348d65f71cd1509234ae8b0debb84
mimcmahon20 <mimcmahon20@vt.edu>
2023-06-01 17:50:32 -0400

    Update AlbumCover.js

--- src/AlbumCover.js
+++ src/AlbumCover.js
@@ -4,7 +4,7 @@ import "./AlbumCover.css";
 function AlbumCover(props) {
     return (
         <div className="AlbumCover">
-            <img className="Cover" src={props.src}/>
+            <img className="Cover" src={props.src} alt="album cover"/>
         </div>
     ); 
 };
